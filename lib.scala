ca70cd8e9099f2d6e7d8ebfe93f6bf0c1f8bffdc
Paweł Marks <pmarks@virtuslab.com>
2023-03-06 09:02:56 +0100

    Implment full trip of query

--- lib.scala
+++ lib.scala
@@ -1,18 +1,33 @@
 //> using scala "3.3.0-RC3"
 //> using option "--explain"
 //> using toolkit "0.1.4"
+//> using dep "com.lihaoyi::requests:0.8.0"
 
 package pytanie
 
 import quoted.*
 import pytanie.parser.parseQuery
 import pytanie.model.*
+import requests.RequestAuth
+import ujson.Str
+import ujson.Obj
+import ujson.Arr
+import ujson.Num
+import ujson.False
+import ujson.True
 
-class PreparedQuery[T]:
-  def get: T = ???
+class PreparedQuery[T](text: String):
+  def send(url: String, auth: RequestAuth): T =
+    val data = ujson.Obj("query" -> text)
+    val response = requests.post(url, auth = auth, data = data.toString).text()
+    Result(ujson.read(response)("data")).asInstanceOf[T]
 
-class Result extends Selectable:
-  def selectDynamic(name: String): Any = new Result
+class Result(data: ujson.Value) extends Selectable:
+  def selectDynamic(name: String): Any =
+    data(name) match
+      case Str(value) => value
+      case Arr(value) => value.map(Result(_)).toList
+      case value      => Result(value)
 
 extension (inline con: StringContext)
   transparent inline def query() = ${ queryImpl('con) }
@@ -25,12 +40,15 @@ private def queryImpl(con: Expr[StringContext])(using Quotes): Expr[Any] =
       val typ =
         val inner = f.selectionSet match
           case Some(selSet) => prepareType(selSet)
-          case None => TypeRepr.of[String]
-        if Set("nodes", "edges").contains(f.name) then TypeRepr.of[List].appliedTo(inner) else inner
+          case None         => TypeRepr.of[String]
+        if Set("nodes", "edges").contains(f.name) then
+          TypeRepr.of[List].appliedTo(inner)
+        else inner
       Refinement(acc, f.name, typ)
 
   val text = con match
     case '{ StringContext($t) } => t.valueOrAbort.stripMargin
 
   prepareType(parseQuery(text).get.selectionSet).asType match
-    case '[t] => '{ new PreparedQuery[t] }
+    case '[t] => '{ new PreparedQuery[t](${ Expr(text) }) }
+end queryImpl
--- playground.scala
+++ playground.scala
@@ -1,21 +1,39 @@
 package pytanie
 
-def myQuery = query"""
-      |query {
-      |  repository(name: "dotty", owner: "lampepfl") {
-      |    issues(first: 10, orderBy: {field: CREATED_AT, direction: DESC}) {
-      |      nodes {
-      |        title
-      |        labels(first: 10) {
-      |          nodes {
-      |           name
-      |          }
-      |        }
-      |      }
-      |    }
-      |  }
-      |}"""
+import requests.RequestAuth
+import java.nio.file.{Files, Paths}
 
-val x = myQuery.get.repository.issues.nodes.flatMap: i =>
-      i.labels.nodes
+@main def playground =
+  val myQuery = query"""
+    |{
+    |  repository(name: "dotty", owner: "lampepfl") {
+    |    issues(
+    |      first: 15,
+    |      filterBy: {states: OPEN},
+    |      orderBy: {field: CREATED_AT, direction: DESC}
+    |    ) {
+    |      nodes {
+    |        number
+    |        title
+    |        labels(first: 10) {
+    |          nodes {
+    |            name
+    |          }
+    |        }
+    |      }
+    |    }
+    |  }
+    |}
+  """
+  val auth = RequestAuth.Basic(
+    "Kordyjan",
+    Files.readAllLines(Paths.get("../keyfile.txt")).get(0)
+  )
+  val res = myQuery.send("https://api.github.com/graphql", auth)
 
+  val x = res.repository.issues.nodes
+    .map: i =>
+      i.number
+    .toSet
+
+  println(x)
