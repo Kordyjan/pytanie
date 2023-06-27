0aaeae758ae354aed1b689029813dce80e569f08
Paweł Marks <pmarks@virtuslab.com>
2023-06-27 15:57:26 +0200

    Setup the first release

--- project.scala
+++ project.scala
@@ -1,6 +1,18 @@
 //> using scala 3.3.0
+//> using jvm "8"
 
 //> using option -Wunused:all -Wvalue-discard
 //> using toolkit 0.2.0
 
 //> using exclude playground.scala
+
+//> using publish.organization "pro.kordyjan"
+//> using publish.name "pytanie"
+//> using publish.computeVersion "git:tag"
+
+//> using publish.repository "central-s01"
+
+//> using publish.license "Apache-2.0"
+//> using publish.url "https://github.com/Kordyjan/pytanie"
+//> using publish.vcs "github:Kordyjan/pytanie"
+//> using publish.developer "Kordyjan|Paweł Marks|https://github.com/Kordyjan"
