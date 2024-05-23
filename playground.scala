fe3e910bb9f84ce0f54c2b983a128bfe70bf6667
Paweł Marks <pmarks@virtuslab.com>
2024-05-23 11:12:24 +0200

    Version update

--- playground.scala
+++ playground.scala
@@ -1,6 +1,6 @@
 //> using files src debugging.scala
-//> using scala 3.3.0
-//> using toolkit latest
+//> using scala 3.3.3
+//> using toolkit 0.4.0
 //> using options -Xcheck-macros
 
 package pytanie
--- project.scala
+++ project.scala
@@ -1,8 +1,8 @@
-//> using scala 3.3.0
+//> using scala 3.3.3
 //> using jvm "11"
 
 //> using option -Wunused:all -Wvalue-discard
-//> using toolkit 0.2.1
+//> using toolkit 0.4.0
 
 //> using exclude playground.scala
 
